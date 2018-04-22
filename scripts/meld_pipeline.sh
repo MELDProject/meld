4c5744c6e14549b2c8adcfc670c353c39be13e50
Sophie <sophie@mc244-125.ich.ucl.ac.uk>
2018-04-22 16:09:03 +0100

    adding removal of old hdf5 files

--- scripts/meld_pipeline.sh
+++ scripts/meld_pipeline.sh
@@ -9,21 +9,21 @@ site_code=$2
 
 #register to symmetric fsaverage xhemi
 echo "Creating registration to template surface"
-#bash "$script_dir"create_xhemi.sh "$subjects_dir" "$subject_ids"
+bash "$script_dir"create_xhemi.sh "$subjects_dir" "$subject_ids"
 
 #create basic features
 echo "Sampling features in native space"
-#bash "$script_dir"sample_FLAIR_smooth_features.sh "$subjects_dir" "$subject_ids" "$script_dir"
+bash "$script_dir"sample_FLAIR_smooth_features.sh "$subjects_dir" "$subject_ids" "$script_dir"
 echo "Filtering intrinsic curvature"
-#python "$script_dir"filter_intrinsic_curvature.py "$subjects_dir" "$subject_ids"
+python "$script_dir"filter_intrinsic_curvature.py "$subjects_dir" "$subject_ids"
 
 #normalise intrasubject
 echo "Intrasubject normalisation"
-#python "$script_dir"intrasubject_normalisation.py "$subjects_dir" "$subject_ids"
+python "$script_dir"intrasubject_normalisation.py "$subjects_dir" "$subject_ids"
 echo "Moving features to template surface"
-#bash "$script_dir"move_to_xhemi_flip.sh "$subjects_dir" "$subject_ids"
+bash "$script_dir"move_to_xhemi_flip.sh "$subjects_dir" "$subject_ids"
 echo "Moving lesion masks to template surface"
-#bash "$script_dir"lesion_labels.sh "$subjects_dir" "$subject_ids"
+bash "$script_dir"lesion_labels.sh "$subjects_dir" "$subject_ids"
 
 #calculates if there are enough (>25) controls
 #create the mu and std overlays for  normalisation of controls and patients
@@ -37,4 +37,6 @@ python "$script_dir"normalise_by_controls.py "$subjects_dir" "$subject_ids" "$sc
 
 #create training_data matrix for all patients and controls.
 echo "creating final training data matrix"
+
+rm "$subjects_dir"MELD_*.hdf5
 python "$script_dir"create_training_data_hdf5.py "$subjects_dir" "$subject_ids"
