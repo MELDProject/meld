ea39fa655b3e636e06df070b61c7b74a5bb2b132
kwagstyl <kw350@cam.ac.uk>
2018-05-18 08:19:18 +0100

    path join for all lines

--- scripts/intrasubject_normalisation.py
+++ scripts/intrasubject_normalisation.py
@@ -26,7 +26,7 @@ args = parser.parse_args()
 #save subjects dir and subject ids. import the text file containing subject ids
 subject_dir=args.subject_dir
 subject_ids_filename=args.subject_ids
-subject_ids=np.loadtxt(subject_dir+subject_ids_filename, dtype='str')
+subject_ids=np.loadtxt(os.path.join(subject_dir,subject_ids_filename), dtype='str')
 
 measures=['.thickness.sm10.mgh', '.w-g.pct.sm10.mgh',
     '.pial.K_filtered.sm20.mgh']
@@ -34,23 +34,23 @@ measures=['.thickness.sm10.mgh', '.w-g.pct.sm10.mgh',
 flair_measures=['.gm_FLAIR_0.sm10.mgh','.gm_FLAIR_0.25.sm10.mgh','.gm_FLAIR_0.5.sm10.mgh',
     '.gm_FLAIR_0.75.sm10.mgh','.wm_FLAIR_0.5.sm10.mgh','.wm_FLAIR_1.sm10.mgh']
 hemis=['lh','rh']
-demo=nb.load(subject_dir+subject_ids[0]+'/surf_meld/'+hemis[0]+measures[0])
+demo=nb.load(os.path.join(subject_dir,subject_ids[0],'surf_meld',hemis[0]+measures[0]))
 
 for h in hemis:
     for s in subject_ids:
-        demo=nb.load(subject_dir+s+'/surf_meld/'+h+measures[0])
-        cortex=nb.freesurfer.io.read_label(subject_dir + s + '/label/'+h+'.cortex.label')
+        demo=nb.load(os.path.join(subject_dir,s,'surf_meld',h+measures[0]))
+        cortex=nb.freesurfer.io.read_label(os.path.join(subject_dir , s , 'label', h+'.cortex.label')
         for m in measures:
-            if not os.path.isfile(subject_dir + s + '/surf_meld/'+h+'.intra_z'+m):
-                subject_measure=io.load_mgh(subject_dir+s+'/surf_meld/'+h+m)
+            if not os.path.isfile(os.path.join(subject_dir , s , 'surf_meld',h+'.intra_z'+m):
+                subject_measure=io.load_mgh(os.path.join(subject_dir,s,'surf_meld',h+m)
                 z_measure=(subject_measure-np.mean(subject_measure[cortex]))/np.std(subject_measure[cortex])
-                io.save_mgh(subject_dir+s+'/surf_meld/'+h+'.intra_z'+m,z_measure,demo)
+                io.save_mgh(os.path.join(subject_dir,s,'surf_meld',h+'.intra_z'+m,z_measure,demo)
         #only do FLAIR measures if present
-        if os.path.isfile(subject_dir + s + '/surf_meld/'+h+flair_measures[0]):
+        if os.path.isfile(os.path.join(subject_dir , s , 'surf_meld',h+flair_measures[0]):
             for m in flair_measures:
-                if not os.path.isfile(subject_dir + s + '/surf_meld/'+h+'.intra_z'+m):
-                    subject_measure=io.load_mgh(subject_dir+s+'/surf_meld/'+h+m)
+                if not os.path.isfile(os.path.join(subject_dir , s , 'surf_meld',h+'.intra_z'+m):
+                    subject_measure=io.load_mgh(os.path.join(subject_dir , s, 'surf_meld',h+m)
                     z_measure=(subject_measure-np.mean(subject_measure[cortex]))/np.std(subject_measure[cortex])
-                    io.save_mgh(subject_dir+s+'/surf_meld/'+h+'.intra_z'+m,z_measure,demo)
+                    io.save_mgh(os.path.join(subject_dir , s, 'surf_meld', h+'.intra_z'+m,z_measure,demo)
 
 
